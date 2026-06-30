641f7f52ed128e398bc07ba011189f1f8a2ba12e
Chenyouli <2642787638@qq.com>
2026-06-30 21:57:15 +0800

    "hyc630"leetcode015question

--- CodeTopic/array_class.py
+++ CodeTopic/array_class.py
@@ -1,4 +1,5 @@
-
+# Hot 100 中的一些题目
+# 01 两数之和
 class Solution:
     # 遍历求解
     def twoSum(self, nums: list[int], target: int) -> list[int]:
@@ -14,23 +15,48 @@ class Solution2:
         for i, item in enumerate(nums):
             cache[item] = i
         # cache = {key: value, key2: value2, ....}
+        # cache 中 key: i(下表0 1 2 3 )  value: nums (nums[0], nums[1]...)
         for i, item in enumerate(nums):
             other = target - item
             if other in cache and cache[other] != i:
                 return [i, cache[other]]
+ 
+
+# 15 三数之和
+'''
+题目中没有明确说明 三元组的顺序
+i < j < k
+答案中不能重复出现 三元组  nums[-1, 0, 1, 2, -1, -4]  不能出现两个[-1, 0, 1]
+'''
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        ans = []
+        n = len(nums)
+        for i in range(n-2):
+            x = nums[i]
+            if i > 0 and x == nums[i-1]:
+                continue
+            j = i + 1
+            k = n - 1
+            while j < k:
+                s = x + nums[j] + nums[k]
+                if s > 0:
+                    k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    ans.append([x, nums[j], nums[k]])
+                    j += 1
+                    while j <k and nums[j] == nums[j-1]:
+                        j += 1
+                    k -= 1
+                    while k> j and nums[k] == nums[k+1]:
+                        k -= 1
+        return ans 
 
 
-
-
-
-
-nums_1 = [1,2,3,6]
-tag = 5
-
-if __name__ == '__main__':
-    s = Solution()
-    ans = s.twoSum(nums_1, tag)
-    print(ans) 
 
 
 
